9026fc64721e83b1da8b3393ff59449baae3469d
airhorse83 <airhorse83@yahoo.com>
2019-03-22 15:55:03 -0700

    nav-container

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import moment from "moment";
-import Portfolio from "./portfolio/portfolio-container";
+
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Tyler Horsley's Porfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}<Portfolio />
+          <NavigationContainer />
+          <h1>Tyler Horsley's Porfolio</h1>
+          {moment().format('MMMM Do YYYY, h:mm:ss a')}
+          <PortfolioContainer />
           
           
-        </div>
+        
         
       </div>
     );
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,19 +3,31 @@ import React, { Component } from 'react';
 import PortfolioItem from "./portfolio-item";
 
 
-export default class Portfolio extends Component {
+export default class PortfolioContainer extends Component {
   constructor() {
     super();
 
     this.state = {
       pageTitle: "Welcome Welcome",
+      isLoading: false,
       data: [
-          {title: "Skaled Sandbox"}, 
-          {title: "Quip"},
-          {title: "Google"},
-          {title: "Apple"}
+          {title: "Skaled Sandbox", category: "Education" }, 
+          {title: "Quip", category: "eCommerce" },
+          {title: "Google", category: "Media"},
+          {title: "Apple", category: "Media"}
         ]
     };
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    })
   }
 
   portfolioItems() {
@@ -24,10 +36,20 @@ export default class Portfolio extends Component {
     })
   }
 
+  
+
   render() {
+    if (this.state.isLoading) {
+      return <div>loading...</div>
+    }
+
     return (
       <div>
           <h2>{this.state.pageTitle}</h2>
+
+          <button onClick={() => this.handleFilter("Media")}>Media</button>
+          <button onClick={() => this.handleFilter("eCommerce")}>eCommerce</button>
+          <button onClick={() => this.handleFilter("Education")}>Education</button>
           
           {this.portfolioItems()}
           
