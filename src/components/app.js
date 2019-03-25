a7aacc60d94ebe8d381ff56643e58a8c7c4960af
airhorse83 <airhorse83@yahoo.com>
2019-03-25 15:05:29 -0700

    catch all for routes done'

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,7 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
   render() {
@@ -29,6 +30,7 @@ export default class App extends Component {
                 <Route path="/contact" component={Contact} />
                 <Route path="/blog" component={Blog} />
                 <Route path="/portfolio/:slug" component={PortfolioDetail} />
+                <Route component={NoMatch} />
                 
               </Switch>
             </div>
