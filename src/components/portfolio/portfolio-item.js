a07c403a138c5f81df3da6f69f13631b9120783c
airhorse83 <airhorse83@yahoo.com>
2019-03-26 13:52:35 -0700

    using personal API data

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -6,6 +6,8 @@ export default function(props) {
     return (
       <div>
           <h3>{props.title}</h3>
+          <h4>{props.description}</h4>
+          <h5>{props.url}</h5>
 
           <Link to={`/portfolio/${props.slug}`}>Link</Link>
       </div>
